a8bd37c4ac3fb672e655ceb686bd7cfe5434be39
h32109 <h32109@gmail.com>
2020-01-14 13:57:13 +0900

    20200114 JPA작업본
    JPA작업 초안

---
+++ src/main/java/com/rpm/web/contents/CarsRepository.java
@@ -0,0 +1,9 @@
+package com.rpm.web.contents;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarsRepository extends CrudRepository<Cars, String> {
+
+}
